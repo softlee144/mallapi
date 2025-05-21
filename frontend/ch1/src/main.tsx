c435cab7f264746c8cd3d7a61998eeece43832c6
softlee144 <ockjungbong@gmail.com>
2025-05-21 21:54:13 +0900

    [프론트] 리액트 라우터 설정

--- frontend/ch1/src/main.tsx
+++ frontend/ch1/src/main.tsx
@@ -1,13 +1,7 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <div>Hello World</div>,
-  },
-]);
+import { RouterProvider } from "react-router";
+import router from "./router/root";
 
 createRoot(document.getElementById("root")!).render(
   <RouterProvider router={router}></RouterProvider>
