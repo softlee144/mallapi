6b30ad7c4d5a2254211bb5a26d74d7668d9d1885
softlee144 <ockjungbong@gmail.com>
2025-05-21 22:51:35 +0900

    [프론트] 공통레이아웃 설정

--- frontend/ch1/src/router/root.tsx
+++ frontend/ch1/src/router/root.tsx
@@ -1,26 +1,33 @@
 import { createBrowserRouter } from "react-router";
 
 import { lazy, Suspense } from "react";
+import BasicLayout from "../layouts/basicLayout";
 const Loading = () => <div>Loading....</div>;
 const Main = lazy(() => import("../pages/mainPage"));
 const About = lazy(() => import("../pages/aboutPage"));
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: (
-      <Suspense fallback={<Loading />}>
-        <Main />
-      </Suspense>
-    ),
-  },
-  {
-    path: "about",
-    element: (
-      <Suspense fallback={<Loading />}>
-        <About />
-      </Suspense>
-    ),
+    path: "",
+    Component: BasicLayout,
+    children: [
+      {
+        index: true,
+        element: (
+          <Suspense fallback={<Loading />}>
+            <Main />
+          </Suspense>
+        ),
+      },
+      {
+        path: "about",
+        element: (
+          <Suspense fallback={<Loading />}>
+            <About />
+          </Suspense>
+        ),
+      },
+    ],
   },
 ]);
 
