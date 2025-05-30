06ec46a4e2c93a0155f450557b152b0bebb59de5
softlee144 <ockjungbong@gmail.com>
2025-05-30 13:28:41 +0900

    [백엔드] 페이지 처리를 위한 DTO 설계

--- apiserver/src/main/java/org/devock/apiserver/dto/PageRequestDTO.java
+++ apiserver/src/main/java/org/devock/apiserver/dto/PageRequestDTO.java
@@ -1,9 +1,11 @@
 package org.devock.apiserver.dto;
 
 import lombok.Builder;
+import lombok.Data;
 import lombok.experimental.SuperBuilder;
 
 @SuperBuilder
+@Data
 public class PageRequestDTO {
 
     @Builder.Default
