b1dd23add1b72b2c513dca409eecf99945ed077c
softlee144 <ockjungbong@gmail.com>
2025-05-31 13:11:04 +0900

    [백엔드] 수정, 삭제 및 CORS 설정

--- apiserver/src/main/java/org/devock/apiserver/config/CustomServletConfig.java
+++ apiserver/src/main/java/org/devock/apiserver/config/CustomServletConfig.java
@@ -3,12 +3,14 @@ package org.devock.apiserver.config;
 import org.devock.apiserver.controller.formatter.LocalDateFormatter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import lombok.extern.log4j.Log4j2;
 
 @Configuration
 @Log4j2
+// WebMvcConfigurer 인터페이스를 추가해 놓음으로써 편하게 오버라이드한다.
 public class CustomServletConfig implements WebMvcConfigurer {
 
     @Override
@@ -18,6 +20,16 @@ public class CustomServletConfig implements WebMvcConfigurer {
         log.info("addFormatter");
 
         registry.addFormatter(new LocalDateFormatter());
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("HEAD", "GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .maxAge(300)
+                .allowedHeaders("Authorization", "Cache-Control", "Content-Type");
     }
 
 }
--- apiserver/src/main/java/org/devock/apiserver/controller/TodoController.java
+++ apiserver/src/main/java/org/devock/apiserver/controller/TodoController.java
@@ -7,6 +7,7 @@ import org.devock.apiserver.dto.PageResponseDTO;
 import org.devock.apiserver.dto.TodoDTO;
 import org.devock.apiserver.service.TodoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
@@ -47,6 +49,30 @@ public class TodoController {
         Long tno = todoService.register(dto);
 
         return Map.of("TNO", tno);
+    }
+
+    @PutMapping("/{tno}")
+    public Map<String, String> modify(
+            @PathVariable(name = "tno") Long tno,
+            @RequestBody TodoDTO todoDTO) {
+
+        todoDTO.setTno(tno);
+
+        log.info("Modify: " + todoDTO);
+
+        todoService.modify(todoDTO);
+
+        return Map.of("RESULT", "SUCCESS");
+    }
+
+    @DeleteMapping("/{tno}")
+    public Map<String, String> remove(@PathVariable(name = "tno") Long tno) {
+
+        log.info("Remove:  " + tno);
+
+        todoService.remove(tno);
+
+        return Map.of("RESULT", "SUCCESS");
     }
 
 }
